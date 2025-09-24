90b74ee5fdc41d1f3bc7e816b6a6050f5d0e25af
Park-seungg <parkis1101@gmail.com>
2025-09-24 11:53:13 +0900

    {IJ} 백엔드 작업

--- backend/src/main/java/com/back/domain/post/post/dto/PostDto.java
+++ backend/src/main/java/com/back/domain/post/post/dto/PostDto.java
@@ -1,14 +1,15 @@
 package com.back.domain.post.post.dto;
 
 import com.back.domain.post.post.entity.Post;
+import org.springframework.lang.NonNull;
 
 import java.time.LocalDateTime;
 
-public record PostDto(long id,
-                      LocalDateTime createDate,
-                      LocalDateTime modifyDate,
-                      String title,
-                      String content) {
+public record PostDto(@NonNull long id,
+                      @NonNull LocalDateTime createDate,
+                      @NonNull LocalDateTime modifyDate,
+                      @NonNull String title,
+                      @NonNull String content) {
     public PostDto(Post post) {
         this(
                 post.getId(),
