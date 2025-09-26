9264e738482a18be4ef46a659facc6321ab96a74
Park-seungg <parkis1101@gmail.com>
2025-09-26 11:23:13 +0900

    {VSC} 실행 오류 해결

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 
 import { Geist, Geist_Mono } from "next/font/google"
 
-import ContextLayout from "./ClientLayout"
+import ContextLayout from "./ContextLayout"
 import "./globals.css"
 
 const geistSans = Geist({
