064bd3feb48a3b64356c234c938f33f1fd64301d
Park-seungg <parkis1101@gmail.com>
2025-09-29 14:45:09 +0900

    {IJ} 네이버 로그인 추가

--- backend/src/main/java/com/back/global/security/CustomOAuth2UserService.java
+++ backend/src/main/java/com/back/global/security/CustomOAuth2UserService.java
@@ -4,27 +4,29 @@ package com.back.global.security;
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final MemberService memberService;
 
+    @Transactional
     // 카카오톡 로그인이 성공할 때 마다 이 함수가 실행된다.
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
-        String oauthUserId = oAuth2User.getName();
+        String oauthUserId = "";
         String providerTypeCode = userRequest.getClientRegistration().getRegistrationId().toUpperCase();
 
         String nickname = "";
@@ -32,20 +34,30 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         String username = "";
 
         switch (providerTypeCode) {
-            case "KAKAO":
+            case "KAKAO" -> {
+                oauthUserId =  oAuth2User.getName();
                 Map<String, Object> attributes = oAuth2User.getAttributes();
                 Map<String, Object> attributesProperties = (Map<String, Object>) attributes.get("properties");
 
                 nickname = (String) attributesProperties.get("nickname");
-                profileImgUrl = (String) attributesProperties.get("profile_image");
-                break;
-            case "GOOGLE":
+                profileImgUrl= (String) attributesProperties.get("profile_image");
+            }
+            case "GOOGLE" -> {
+                oauthUserId =  oAuth2User.getName();
                 nickname = (String) oAuth2User.getAttributes().get("name");
-                profileImgUrl = (String) oAuth2User.getAttributes().get("picture");
-                break;
-        }
-        username = providerTypeCode + "_" + oauthUserId;
+                profileImgUrl= (String) oAuth2User.getAttributes().get("picture");
+            }
+            case "NAVER" -> {
+                Map<String, Object> attributes = oAuth2User.getAttributes();
+                Map<String, Object> attributesProperties = (Map<String, Object>) attributes.get("response");
 
+                oauthUserId = (String) attributesProperties.get("id");
+                nickname = (String) attributesProperties.get("nickname");
+                profileImgUrl= (String) attributesProperties.get("profile_image");
+            }
+        }
+
+        username = providerTypeCode + "__%s".formatted(oauthUserId);
         String password = "";
 
         Member member = memberService.modifyOrJoin(username, password, nickname, profileImgUrl).data();
